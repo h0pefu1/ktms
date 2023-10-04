d76a8ff50d0e651527301cd08a2995fb08c77ccb
ringo1206 <romavoitenko195@gmail.com>
2023-10-04 17:42:06 +0300

    some changes

--- front/src/components/sidebar/index.tsx
+++ front/src/components/sidebar/index.tsx
@@ -23,9 +23,9 @@ const Sidebar = (props: {
         <HiX />
       </span>
 
-      <div className={`mx-[56px] mt-[50px] flex items-center`}>
+      <div className={`mx-[80px] mt-[50px] flex items-center`}>
         <div className="mt-1 ml-1 h-2.5 font-poppins text-[26px] font-bold uppercase text-navy-700 dark:text-white">
-          AMAP <span className="font-medium"> APP</span>
+          KTMS
         </div>
       </div>
       <div className="mt-[58px] mb-7 h-px bg-gray-300 dark:bg-white/30" />
